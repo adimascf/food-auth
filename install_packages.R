df0e1bad29f08d9de288d8ff86f2f5c2fe775466
adimascf <cahyaningdimas@gmail.com>
2024-04-30 11:13:12 +0700

    correct the r packages installation script

--- install_packages.R
+++ install_packages.R
@@ -1,9 +1,13 @@
 # Define a list of packages and their versions
-packages <- c("tidyverse", "readxl", "rmarkdown", "ggtext", "RColorBrewer", "formattable", "knitr")
-versions <- c("2.0.0", "1.4.3", "2.25", "0.1.2", "1.1.3", "0.2.1", "1.44")
+packages <- c("tidyverse", "readxl", "rmarkdown", "ggtext", 
+              "RColorBrewer", "formattable", "knitr", "gifski")
+versions <- c("2.0.0", "1.4.3", "2.25", "0.1.2", "1.1.3", "0.2.1", "1.44", "1.12.2")
 
 # Loop through the packages and install each one with the specified version
-devtools::install_github("r-rust/gifski")
+
+install.packages(packages[length(packages)], version = versions[length(versions)])
+
+#devtools::install_github("r-rust/gifski")
 for (i in seq_along(packages)) {
   install.packages(packages[i], version = versions[i], dependencies = TRUE)
   print(paste0(packages[i], " Installation Completed"))
